a3c1e5f5e6dcc1466aa6f66dc6e78b185a749d40
Musayeva Zibeyda Rovshan <Musay_zv97@itstep.edu.az>
2025-01-13 11:44:10 +0400

    lesson 1

---
+++ ASP_Lesson1_Introduction/ASP_Lesson1_Introduction/Program.cs
@@ -0,0 +1,28 @@
+﻿
+using ASP_Lesson1_Introduction;
+using System.Net;
+using System.Text;
+
+
+#region HttpListener
+
+//HttpListener listener = new HttpListener();
+
+//listener.Prefixes.Add("http://localhost:27001/");
+//listener.Start();
+
+//Console.WriteLine("I am listening");
+
+//while (true)
+//{
+//    HttpListenerContext context = listener.GetContext();
+//    Console.WriteLine("Request handle");
+//    context.Response.OutputStream.Write(Encoding.UTF8.GetBytes("Hello world"));
+//    context.Response.Close();   
+//}
+
+#endregion
+
+
+Webhost myHost = new(27001);
+myHost.Run();
---
+++ ASP_Lesson1_Introduction/ASP_Lesson1_Introduction/Webhost.cs
@@ -0,0 +1,87 @@
+﻿using System.Net;
+
+namespace ASP_Lesson1_Introduction;
+
+public class Webhost
+{
+    private short _port;
+    private HttpListener _listener;
+
+    public Webhost(short port)
+    {
+        _port = port;
+    }
+
+    public void Run()
+    {
+        _listener = new HttpListener();
+        _listener.Prefixes.Add($"http://localhost:{_port}/");
+        _listener.Start();
+        Console.WriteLine($"Http server started on {_port}");
+
+        while ( true )
+        {
+            HttpListenerContext context = _listener.GetContext();
+
+            Task.Run(() =>
+            {
+                HandlerRequest(context );
+            });
+        }
+    }
+
+    private void HandlerRequest(HttpListenerContext context)
+    {
+        HttpListenerRequest request = context.Request;
+        HttpListenerResponse response = context.Response;   
+
+        var writer = new StreamWriter(response.OutputStream);
+        
+        var str = request.RawUrl;
+        var path = str.Substring(1);
+
+        Console.WriteLine(path);
+
+        if (path == "")
+            path = "index";
+
+        if (!Path.HasExtension(path))
+            path += ".html";
+
+        var fileName = "../../../Views/" + path;
+
+        try
+        {
+            if (File.Exists(fileName))
+            {
+                response.StatusCode = 200;
+                response.ContentType = "text/html";
+                var text = File.ReadAllText(fileName);
+                writer.Write(text);
+            }
+
+            else
+            {
+                response.StatusCode = 200;
+                var text = File.ReadAllText("../../../Views/404.html");
+                writer.Write(text);
+            }
+        }
+        catch (Exception ex)
+        {
+            response.StatusCode = 500;
+            writer.Write(ex.ToString());
+        }
+
+        finally
+        {
+            writer.Dispose();
+        }
+
+    
+
+
+
+
+    }
+}
